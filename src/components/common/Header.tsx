fc1adc75a145d3be442708a7aa21065b41b678a9
eunbinnie <dmsqlsdl54@gmail.com>
2025-01-28 16:19:26 +0900

    feat: 헤더 컴포넌트 작업

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from 'public/icons/logo.svg';
+import Profile from 'public/icons/profile.svg';
+
 const Header = () => {
-  return <header className='container'>헤더</header>;
+  return (
+    <header className='container flex items-center justify-between gap-1 pb-[6px] pt-[18px] sm:py-[30px]'>
+      <div className='size-8' />
+      <Link
+        href={'/list'}
+        className='relative aspect-[525/100] w-[108px] sm:w-[120px] lg:w-[168px]'
+      >
+        <Image src={Logo} alt='FANDOM-K' fill priority sizes='max-width:100%' />
+      </Link>
+      <Link href={'/mypage'}>
+        <Image src={Profile} alt='마이페이지' width={32} height={32} priority />
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
