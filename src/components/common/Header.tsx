721d4b2552baf83b19b374538342b5983c527ee1
eunbinnie <dmsqlsdl54@gmail.com>
2025-01-28 18:38:36 +0900

    refactor: 로고 h1 태그로 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,12 +7,17 @@ const Header = () => {
   return (
     <header className='container flex items-center justify-between gap-1 pb-[6px] pt-[18px] sm:py-[30px]'>
       <div className='size-8' />
-      <Link
-        href={'/list'}
-        className='relative aspect-[525/100] w-[108px] sm:w-[120px] lg:w-[168px]'
-      >
-        <Image src={Logo} alt='FANDOM-K' fill priority sizes='max-width:100%' />
-      </Link>
+      <h1 className='relative aspect-[525/100] w-[108px] sm:w-[120px] lg:w-[168px]'>
+        <Link href={'/list'} className='block h-full'>
+          <Image
+            src={Logo}
+            alt='FANDOM-K'
+            fill
+            priority
+            sizes='max-width:100%'
+          />
+        </Link>
+      </h1>
       <Link href={'/mypage'}>
         <Image src={Profile} alt='마이페이지' width={32} height={32} priority />
       </Link>
