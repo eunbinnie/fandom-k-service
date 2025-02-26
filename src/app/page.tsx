3d89eadbfb458308b3549179c293eed47b3649c1
eunbinnie <dmsqlsdl54@gmail.com>
2025-02-26 20:19:04 +0900

    refactor: 랜딩페이지 콘텐츠 데이터 상수 파일 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,12 @@
-/* eslint-disable @next/next/no-img-element */
 'use client';
 
+import { LANDING_CONTENTS } from '@/constants/landing.constants';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import LogoImage from 'public/icons/logo.svg';
 import HeaderBackgroundImage from 'public/images/HeaderBackground.png';
 
 import Button from '@/components/button/Button';
-
-const LANDING_CONTENTS = [
-  {
-    title: '후원하기',
-    contents1: '좋아하는 아이돌에게',
-    contents2: '쉽게 조공해 보세요',
-    backgroundImage: `url('/images/MainBackground_1.png')`,
-    image: '/images/MainContent_1.png',
-  },
-  {
-    title: '이달의 아티스트',
-    contents1: '내 아티스트에게 1등의',
-    contents2: '영예를 선물하세요',
-    backgroundImage: `url('/images/MainBackground_2.png')`,
-    image: '/images/MainContent_2.png',
-  },
-  {
-    title: '나만의 아티스트',
-    contents1: '좋아하는 아티스트들의',
-    contents2: '소식을 모아보세요',
-    backgroundImage: `url('/images/MainBackground_3.png')`,
-    image: '/images/MainContent_3.png',
-  },
-];
 
 export default function LandingPage() {
   const router = useRouter();
---
+++ src/constants/landing.constants.ts
@@ -0,0 +1,23 @@
+export const LANDING_CONTENTS = [
+  {
+    title: '후원하기',
+    contents1: '좋아하는 아이돌에게',
+    contents2: '쉽게 조공해 보세요',
+    backgroundImage: `url('/images/MainBackground_1.png')`,
+    image: '/images/MainContent_1.png',
+  },
+  {
+    title: '이달의 아티스트',
+    contents1: '내 아티스트에게 1등의',
+    contents2: '영예를 선물하세요',
+    backgroundImage: `url('/images/MainBackground_2.png')`,
+    image: '/images/MainContent_2.png',
+  },
+  {
+    title: '나만의 아티스트',
+    contents1: '좋아하는 아티스트들의',
+    contents2: '소식을 모아보세요',
+    backgroundImage: `url('/images/MainBackground_3.png')`,
+    image: '/images/MainContent_3.png',
+  },
+];
