40edf8a76bc3e3e6ce45f42304eb4af10c171b58
eunbinnie <dmsqlsdl54@gmail.com>
2025-02-27 16:41:32 +0900

    feat: 충전 기능 개발 및 크레딧 아이템 컴포넌트 분리

--- src/app/(route)/list/_components/ChargeCreditModal.tsx
+++ src/app/(route)/list/_components/ChargeCreditModal.tsx
@@ -1,52 +1,39 @@
 'use client';
 
+import { useState } from 'react';
+
 import { useCreditStore } from '@/store';
 import Image from 'next/image';
-import CreditIcon from 'public/icons/credit.svg';
 import WhiteCreditIcon from 'public/icons/white-credit.svg';
 
 import { cn } from '@/lib/styleUtils';
 
 import Button from '@/components/button/Button';
 
-const CREDIT_POINT = [
-  { point: 100, active: true },
-  { point: 500, active: false },
-  { point: 1000, active: false },
-];
+import CreditItem from './CreditItem';
+
+const CREDIT_POINT = [{ point: 100 }, { point: 500 }, { point: 1000 }];
 
 const ChargeCreditModal = () => {
   const chargeCredit = useCreditStore((state) => state.addCredit);
+  const [selectedCredit, setSelectedCredit] = useState<number>(
+    CREDIT_POINT[0].point,
+  );
 
   return (
     <div className='mt-6'>
       <fieldset className='grid gap-2'>
         {CREDIT_POINT.map((item) => (
-          <label
+          <CreditItem
             key={item.point}
-            htmlFor={`${item.point}point`}
-            className={cn(
-              'flex items-center justify-between gap-3 rounded-lg border px-5 py-[18px]',
-              item.active ? 'border-brand-orange' : 'border-white-primary',
-            )}
-          >
-            <div className='flex items-center gap-1'>
-              <Image src={CreditIcon} alt='크레딧' width={22} height={26} />
-              <span className='text-xl font-bold leading-[1.3]'>
-                {item.point}
-              </span>
-            </div>
-            <input
-              type='radio'
-              id={`${item.point}point`}
-              name='credit'
-              value={item.point}
-            />
-          </label>
+            credit={item.point}
+            selectedCredit={selectedCredit}
+            onClick={() => setSelectedCredit(item.point)}
+          />
         ))}
         <Button
           type='submit'
-          onClick={() => {}}
+          onClick={() => chargeCredit(selectedCredit)}
           className='mt-6 flex items-center justify-center gap-1 rounded-lg py-2'
         >
           <Image src={WhiteCreditIcon} alt='크레딧' width={21} height={25} />
