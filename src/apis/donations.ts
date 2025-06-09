15e8f0bfa36fe7899f483b0720dd9c0c5d7e5375
eunbinnie <dmsqlsdl54@gmail.com>
2025-06-09 19:22:28 +0900

    feat: 조공 후원 PUT API 작성

--- src/apis/donations.ts
+++ src/apis/donations.ts
@@ -17,3 +17,25 @@ export const getDonations = async () => {
     }
   }
 };
+
+/**
+ * 후원하기
+ * @param donationId 후원 ID
+ * @param amount 후원 데이터
+ */
+export const putDonation = async (donationId: number, amount: number) => {
+  try {
+    const res = await instance.put(`/api/donations/${donationId}/contribute`, {
+      amount,
+    });
+    const { data } = res;
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('An unknown error occurred');
+    }
+  }
+};
